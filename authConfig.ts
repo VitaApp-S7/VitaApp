e9f5ef7236d1839e0fc214e8b1e991bf588521dc
Dylan Nas <432592@student.fontys.nl>
2023-02-26 16:01:42 +0100

    Auth config uses a base url now

--- authConfig.ts
+++ authConfig.ts
@@ -1,66 +1,39 @@
+const baseUrl = "http://145.93.160.94:5000";
+
+//CLOUD
+//const baseUrl = "https://vitaappgw.northeurope.cloudapp.azure.com";
+
 export const protectedResources = {
     graphMe: {
         endpoint: "https://graph.microsoft.com/v1.0/me",
         scopes: ["User.Read"],
     },
     apiActivity: {
-        endpoint: "http://145.93.160.94:5000/moodbooster/",
+        endpoint: baseUrl + "/moodbooster/",
         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"], // e.g. api://xxxxxx/access_as_user
     },
     apiUser: {
-        endpoint: "http://145.93.160.94:5000/user/",
+        endpoint: baseUrl + "/user/",
         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
     },
     apiBadge: {
-        endpoint: "http://145.93.160.94:5000/badge/",
+        endpoint: baseUrl + "/badge/",
         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
     },
     apiChallenge: {
-        endpoint: "http://145.93.160.94:5000/challenge/",
+        endpoint: baseUrl + "/challenge/",
         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
     },
     apiNews: {
-        endpoint: "http://145.93.160.94:5000/feed/",
+        endpoint: baseUrl + "/feed/",
         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
     },
     apiFriends: {
-        endpoint: "http://145.93.160.94:5000/user/friends/",
+        endpoint: baseUrl + "/user/friends/",
         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
     },
     apiEvent: {
-        endpoint:"http://1145.93.160.94:5000/event/",
+        endpoint: baseUrl + "/event/",
         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
     }
 }
-
-// CLOUD
-// export const protectedResources = {
-//     graphMe: {
-//         endpoint: "https://graph.microsoft.com/v1.0/me",
-//         scopes: ["User.Read"],
-//     },
-//     apiActivity: {
-//         endpoint: "https://vitaappgw.northeurope.cloudapp.azure.com/moodbooster/" ,
-//         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"], // e.g. api://xxxxxx/access_as_user
-//     },
-//     apiUser: {
-//         endpoint: "https://vitaappgw.northeurope.cloudapp.azure.com/user/" ,
-//         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
-//     },
-//     apiBadge: {
-//         endpoint: "https://vitaappgw.northeurope.cloudapp.azure.com/badge/",
-//         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
-//     },
-//     apiChallenge: {
-//         endpoint:"https://vitaappgw.northeurope.cloudapp.azure.com/challenge/",
-//         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
-//     },
-//     apiNews: {
-//         endpoint:"https://vitaappgw.northeurope.cloudapp.azure.com/feed/",
-//         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
-//     },
-//     apiEvent: {
-//         endpoint:"https://vitaappgw.northeurope.cloudapp.azure.com/event/",
-//         scopes: ["api://215b09e4-54cb-49aa-837b-546f73fc29f6/User.All"]
-//     }
-// }
