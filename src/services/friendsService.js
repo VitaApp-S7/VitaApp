efb353504bf76b08958700c53c46cd0989a68f5e
Dylan Nas <432592@student.fontys.nl>
2023-03-25 16:30:58 +0100

    Cleaned up components and services

--- src/services/friendsService.js
+++ src/services/friendsService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { protectedResources } from "../../authConfig"
 
-const url = `${protectedResources.apiUser.endpoint  }friends`
+const url = `${protectedResources.apiUser.endpoint}friends`
 
 export async function getFriends(token) {
   var response = await axios.get(url, { headers: { Authorization: `Bearer ${token}` }})
@@ -9,46 +9,45 @@ export async function getFriends(token) {
 }
 
 export async function addFriend(token, id) {
-  const res = await fetch(`${url  }/add/${  id}`, {
+  const res = await fetch(`${url}/add/${id}`, {
     method: "POST",
     headers: {
-      "Accept": "application/json",
+      Accept: "application/json",
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`
+      Authorization: `Bearer ${token}`
     }
   })
   return res
 }
 
-
 export async function removeFriend(token, id) {
-  var response = await axios.delete(`${url  }/remove/${  id}`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.delete(`${url}/remove/${id}`, { headers: { Authorization: `Bearer ${token}` }})
   return response
 }
 
 export async function getFrRequests(token) {
-  var response = await axios.get(`${url  }/requests`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.get(`${url}/requests`, { headers: { Authorization: `Bearer ${token}` }})
   return response
 }
 
 export async function acceptFrRequest(token, friendReqId) {
-  const res = await fetch(`${url  }/requests/accept/${  friendReqId}`, {
+  const res = await fetch(`${url}/requests/accept/${friendReqId}`, {
     method: "POST",
     headers: {
-      "Accept": "application/json",
+      Accept: "application/json",
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`
+      Authorization: `Bearer ${token}`
     }
   })
   return res
 }
 
 export async function cancelFrRequest(token, reqId) {
-  var response = await axios.delete(`${url  }/requests/cancel/${  reqId}`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.delete(`${url}/requests/cancel/${reqId}`, { headers: { Authorization: `Bearer ${token}` }})
   return response
 }
 
 export async function getSendedRequests(token) {
-  var response = await axios.get(`${url  }/sendedrequests`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.get(`${url}/sendedrequests`, { headers: { Authorization: `Bearer ${token}` }})
   return response.data
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -2,78 +2,69 @@ import axios from "axios"
 import { protectedResources } from "../../authConfig"
 import "react-native-url-polyfill/auto"
 
-
 const url = protectedResources.apiUser.endpoint
 
 export async function getAllUsers(token) {
-  var response = await axios.get(`${url  }public/all`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.get(`${url}public/all`, { headers: { Authorization: `Bearer ${token}` }})
   return response.data
 }
 
 export async function setUserExpoPushToken(token) {
-  var response = await axios.get(`${url  }all`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.get(`${url}all`, { headers: { Authorization: `Bearer ${token}` }})
   return response.data
 }
 
 export async function getUser(token) {
-  var response = await axios.get(`${url  }me`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.get(`${url}me`, { headers: { Authorization: `Bearer ${token}` }})
   return response.data
 }
-
 
 export async function checkUser(token) {
-
-  var response = await axios.get(`${url  }login/check`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.get(`${url}login/check`, { headers: { Authorization: `Bearer ${token}` }})
   return response.data
 }
 
-
 export async function SetModalVisable(token, isModalVisable) {
-  const res = await fetch(`${url  }modalvisible/${  isModalVisable}`, {
+  const res = await fetch(`${url}modalvisible/${isModalVisable}`, {
     method: "POST",
     headers: {
-      "Accept": "application/json",
+      Accept: "application/json",
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${ token }`
+      Authorization: `Bearer ${token}`
     }
   })
   return await res.json()
-
 }
 
 export async function GetModalVisable(token) {
-  
-  var response = await axios.get(`${url  }modalvisible`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.get(`${url}modalvisible`, { headers: { Authorization: `Bearer ${token}` }})
   return response.data
 }
 
 export async function GetDate(token) {
-  
-  var response = await axios.get(`${url  }date`, { headers: { Authorization: `Bearer ${token}` }})
+  var response = await axios.get(`${url}date`, { headers: { Authorization: `Bearer ${token}` }})
   return response.data
 }
 
-
 export async function SetDate(token, date) {
-  const res = await fetch(`${url  }date/${  date}`, {
+  const res = await fetch(`${url}date/${date}`, {
     method: "POST",
     headers: {
-      "Accept": "application/json",
+      Accept: "application/json",
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${ token }`
+      Authorization: `Bearer ${token}`
     }
   })
   return await res.json()
-
 }
 
 export async function SetExpo(token, expoToken) {
-  const res = await fetch(`${url  }setexpo/${  expoToken}`, {
+  const res = await fetch(`${url}setexpo/${expoToken}`, {
     method: "POST",
     headers: {
-      "Accept": "application/json",
+      Accept: "application/json",
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${ token }`
+      Authorization: `Bearer ${token}`
     }
   })
   console.log(`${expoToken} written to database`)
@@ -81,14 +72,13 @@ export async function SetExpo(token, expoToken) {
 }
 
 export async function updateUserMood(token, points) {
-  const res = await fetch(`${url  }setmood/${  points}`, {
+  const res = await fetch(`${url}setmood/${points}`, {
     method: "POST",
     headers: {
-      "Accept": "application/json",
+      Accept: "application/json",
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${ token }`
+      Authorization: `Bearer ${token}`
     }
   })
   return await res.json()
-
 }
