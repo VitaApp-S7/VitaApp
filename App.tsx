43da84494c643f259edcea86baa977c54430ee2f
Dylan Nas <432592@student.fontys.nl>
2023-03-26 01:54:05 +0100

    Added better default caching settings

--- App.tsx
+++ App.tsx
@@ -5,13 +5,20 @@ import React from "react"
 import Toast from "react-native-toast-message"
 import Notification from "./src/components/Notifications/Notification"
 import "react-native-url-polyfill/auto"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryClient } from "@tanstack/react-query"
 import { AppProvider } from "./src/context/AppContext"
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 24 * 60 * (60 * 1000),
+      staleTime: 2 * (60 * 1000)
+    }
+  }
+})
 
 const asyncStoragePersister = createAsyncStoragePersister({
   storage: AsyncStorage,
