024ccc08f467532e3f2738a887b91d74090e715d
Dylan Nas <432592@student.fontys.nl>
2023-04-12 16:28:46 +0200

    Merge last demo changes (#19)
    * Rich text viewer in moodbooster details

--- src/components/MoodBoosterDetails.tsx
+++ src/components/MoodBoosterDetails.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Text, View, StyleSheet } from "react-native"
 import { useMoodboosterStartMutation } from "../mutations/MoodboosterMutations"
 import ButtonPrimary from "./ButtonPrimary"
+import RichTextViewer from "./RichTextViewer"
 export const MoodBoosterDetails = ({ moodbooster, setUserMoodbooster }) => {
   const startMutation = useMoodboosterStartMutation(moodbooster.id)
 
@@ -11,19 +12,12 @@ export const MoodBoosterDetails = ({ moodbooster, setUserMoodbooster }) => {
   }
 
   return (
-    <View style={styles.wrapper}>
-      <View style={styles.wrapperTop}>
-        <Text style={styles.title}>{moodbooster.title}</Text>
-      </View>
-
-      <Text style={styles.description}>{moodbooster.description}</Text>
-      <View style={styles.buttons}>
-        <ButtonPrimary
-          text={"START"}
-          disabled={!startMutation.isIdle}
-          onPress={() => handleToStart()}
-        ></ButtonPrimary>
-      </View>
+    <View style={styles.buttons}>
+      <ButtonPrimary
+        text={"START"}
+        disabled={!startMutation.isIdle}
+        onPress={() => handleToStart()}
+      ></ButtonPrimary>
     </View>
   )
 }
@@ -34,29 +28,5 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     paddingRight: 10
-  },
-  wrapper: { margin: 16 },
-  title: {
-    fontFamily: "Poppins600SemiBold",
-    margin: 0,
-    padding: 0,
-    fontSize: 20,
-    color: "#031D29"
-  },
-  description: {
-    fontFamily: "Poppins500Medium",
-    margin: 0,
-    padding: 0,
-    fontSize: 12,
-    color: "#052D40",
-    paddingVertical: 4
-  },
-  wrapperTop: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100%",
-    paddingVertical: 4
   }
 })
--- src/screens/page-moodbooster-details/page-moodbooster-details.tsx
+++ src/screens/page-moodbooster-details/page-moodbooster-details.tsx
@@ -1,10 +1,11 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native"
 import React, { useState } from "react"
 import { useRoute } from "@react-navigation/native"
 import { UserMoodboosterType } from "../../types/MoodboosterTypes"
 import { BoostersNavProps } from "../../navigation/BoostersNav"
 import { UserMoodBoosterDetails } from "../../components/UserMoodBoosterDetails"
 import { MoodBoosterDetails } from "../../components/MoodBoosterDetails"
+import RichTextViewer from "../../components/RichTextViewer"
 
 const PageMoodboosterDetails = () => {
   const route = useRoute<BoostersNavProps<"Moodbooster Details">["route"]>()
@@ -13,23 +14,33 @@ const PageMoodboosterDetails = () => {
     useState<UserMoodboosterType | null>(route.params?.userMb)
 
   return (
-    <ScrollView style={styles.screen}>
+    <View style={styles.screen}>
       <Image
         source={require("../../../assets/header.png")}
         style={styles.header}
       ></Image>
-      {userMoodbooster ? (
-        <UserMoodBoosterDetails
-          userMoodbooster={userMoodbooster}
-          setUserMoodbooster={setUserMoodbooster}
+      <View style={styles.wrapper}>
+        <View style={styles.wrapperTop}>
+          <Text style={styles.title}>{moodbooster.title}</Text>
+        </View>
+
+        <RichTextViewer
+          html={moodbooster.description}
+          queryKey={`mbhtml${moodbooster.id}`}
         />
-      ) : (
-        <MoodBoosterDetails
-          moodbooster={moodbooster}
-          setUserMoodbooster={setUserMoodbooster}
-        />
-      )}
-    </ScrollView>
+        {userMoodbooster ? (
+          <UserMoodBoosterDetails
+            userMoodbooster={userMoodbooster}
+            setUserMoodbooster={setUserMoodbooster}
+          />
+        ) : (
+          <MoodBoosterDetails
+            moodbooster={moodbooster}
+            setUserMoodbooster={setUserMoodbooster}
+          />
+        )}
+      </View>
+    </View>
   )
 }
 
@@ -40,5 +51,21 @@ const styles = StyleSheet.create({
   screen: {
     backgroundColor: "white",
     height: "100%"
+  },
+  wrapper: { margin: 16 },
+  title: {
+    fontFamily: "Poppins600SemiBold",
+    margin: 0,
+    padding: 0,
+    fontSize: 20,
+    color: "#031D29"
+  },
+  wrapperTop: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingVertical: 4
   }
 })
